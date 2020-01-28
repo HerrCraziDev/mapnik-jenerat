c75f45d240072fb3f6bb41819387c928f8b27079
HerrCraziDev <herrcrazi@gmail.com>
2020-01-28 14:48:57 +0100

    Initial commit - test files

---
+++ test.cpp
@@ -0,0 +1,28 @@
+
+
+#include <mapnik/map.hpp>
+#include <mapnik/load_map.hpp>
+#include <mapnik/agg_renderer.hpp>
+#include <mapnik/image.hpp>
+#include <mapnik/image_util.hpp>
+#include <mapnik/plugin.hpp>
+#include <mapnik/datasource_cache.hpp>
+
+
+#define WIDTH 16000
+#define HEIGHT 7000
+
+
+main()
+{
+    mapnik::datasource_cache::instance().register_datasources("/usr/lib/mapnik/3.0/input");
+
+    mapnik::Map m(WIDTH, HEIGHT);
+    mapnik::load_map(m, "test.xml");
+    m.zoom_all();
+    mapnik::image_rgba8 im(WIDTH,HEIGHT);
+    mapnik::agg_renderer<mapnik::image_rgba8> ren(m, im);
+    ren.apply();
+    mapnik::save_to_file(im, "our_world.png");
+}
+
